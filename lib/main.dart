cea9936abe0345ae0a3123149e242e8c886e2f32
sebcrisan <sebastiaancrisan@gmail.com>
2023-05-16 14:27:37 +0200

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// provider scope
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timer App',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Timer App'),
+    );
+  }
+}
+
+// Home page screen
+class MyHomePage extends ConsumerWidget {
+  /// Init homepage
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  /// The title of the homepage
+  final String title;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer'),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
